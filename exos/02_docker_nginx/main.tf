a2b13e56c3cc76f01d04289b132031eeb7ebe700
Mohammed SEDDIK <mbest742@gmail.com>
2025-10-07 16:03:19 +0200

    Exercice 2.1 – Déployer Nginx

---
+++ exos/02_docker_nginx/main.tf
@@ -0,0 +1,29 @@
+# exos/02_docker_nginx/main.tf
+
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"  # Pour Windows
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx-terraform"
+  image = docker_image.nginx.image_id
+  
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
